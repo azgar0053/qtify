a727b30989d3b633b99a41b82e07dc36b8cecb63
Mohd Azgar Ali <azgar53ali@gmail.com>
2023-11-24 12:46:19 +0530

    added card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,58 @@
+import { Chip, Tooltip } from "@mui/material";
+import React from "react";
+import styles from './Card.module.css'
+
+
+const Card=( data, type)=>{
+    const getCard=(type)=>{
+        switch(type){
+            case 'album':{
+                const {image,follows, title, slug, songs} = data;
+                return(
+                    <Tooltip title={`${songs.length} songs`} placement="top" arrow>
+                        <a href={`album/${slug}`}>        
+                            <div className={styles.wrapper}>
+                                <div className={styles.card}>
+                                    <img src={image} alt="album" loading="lazy"/>
+                                    <div className={styles.chipWrapper}>
+                                        <Chip label={`${follows} Follows`}
+                                        size="small"
+                                        className={styles.Chip}/>
+                                    </div>
+                                </div>
+                                <div className={styles.titleText}>
+                                    <p>{title}</p>
+                                </div>
+                            </div>
+                        </a>
+                    </Tooltip>
+                )
+            }
+            case 'song':{
+                const {image, likes, title} = data;
+                return(
+                    <div className={styles.wrapper}>
+                                <div className={styles.card}>
+                                    <img src={image} alt="album" loading="lazy"/>
+                                    <div className={styles.chipWrapper}>
+                                        <Chip label={`${likes} Likes`}
+                                        size="small"
+                                        className={styles.Chip}/>
+                                    </div>
+                                </div>
+                                <div className={styles.titleText}>
+                                    <p>{title}</p>
+                                </div>
+                            </div>
+                )
+
+            }
+            default :return <></>
+        }
+    }
+
+    return getCard(type)    
+ 
+}
+
+export default Card;
